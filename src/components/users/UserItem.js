8c265e1f82dce08a07ecd9ced83147130d02ba14
Rafael Estrada <rafaelestrada@comcast.net>
2021-09-30 21:55:38 -0700

    refactored components

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,11 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
 
-class UserItem extends Component {
+const UserItem = ({user:{login, avatar_url, html_url}}) =>  {
   
 
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
+  
+    
     return (
       <div className="card text-center">
         <img
@@ -22,8 +23,12 @@ class UserItem extends Component {
           </a>
         </div>
       </div>
-    );
-  }
+    
+    )
+}
+
+UserItem.propType = {
+  user:PropTypes.object.isRequired
 }
 
 export default UserItem;
