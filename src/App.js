94948317ab0b3f53cf389c995b410d3c2ecec070
Rafael Estrada <rafaelestrada@comcast.net>
2021-10-16 20:51:54 -0700

    added bio, info

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component{
 state ={
   users:[],
   user:{},
+  repos:[],
   loading:false,
   alert:null
 }
@@ -54,13 +55,29 @@ getUser= async(username) =>{
   this.setState({loading:true});
 
   const res = await axios.get(
-    `https://api.github.com/users/${username}?client_id=${
+    `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
       process.env.REACT_APP_GITHUB_CLIENT_ID
     }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
   );
 this.setState({user:res.data,loading:false});
 
 };
+// Get Users Repos
+getUserRepos= async(username) =>{
+  this.setState({loading:true});
+
+  const res = await axios.get(
+    `https://api.github.com/users/${username}?client_id=${
+      process.env.REACT_APP_GITHUB_CLIENT_ID
+    }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  );
+this.setState({repos:res.data,loading:false});
+
+};
+
+
+
+
 
 
 // Clear Users from State
@@ -76,7 +93,7 @@ setTimeout(() => this.setState({alert:null}),5000);
 
 
   render(){
-    const {users,user,loading}= this.state;
+    const {users,user,repos,loading}= this.state;
    
 
     return (
@@ -109,7 +126,9 @@ setTimeout(() => this.setState({alert:null}),5000);
            <User
             {...props}             
             getUser={this.getUser} 
+            getUserRepos={this.getUserRepos}
             user={user} 
+            repos={repos}
             loading={loading}
             />
          )}
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,23 +1,23 @@
 import React, { Component, Fragment } from "react";
-import Spinner from '../layout/Spinner';
-import PropTypes from 'prop-types';
-import {Link} from 'react-router-dom';
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-
- class User extends Component {
+class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
   }
-  static propTypes ={
-    loading:PropTypes.bool,
-    user:PropTypes.object.isRequired,
-    getUser:PropTypes.func.isRequired
-
-    
-  }
+  static propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired,
+    getUserRepos:PropTypes.func.isRequired
+  };
 
   render() {
     const {
+      company,
       name,
       avatar_url,
       location,
@@ -32,44 +32,77 @@ import {Link} from 'react-router-dom';
       hireable,
     } = this.props.user;
 
-    const {loading} = this.props;
-    
-    if(loading) return <Spinner/>;
+    const { loading } = this.props;
 
-
-
-
-
+    if (loading) return <Spinner />;
 
     return (
-    <Fragment>
- 
-<Link to ='/' className='btn btn-dark'>
-Back to Search
-</Link>
-Hireable: {''}
-{hireable ? ( 
-<i className='fas fa-check text-success'/> 
-) :  (
-<i className='fas fa-times-circle text danger'/>
-)}
-   
-<div className="card grid2">
-  <div className="all-center">
+      <Fragment>
+        <Link to="/" className="btn btn-dark">
+          Back to Search
+        </Link>
+        Hireable: {""}
+        {hireable ? (
+          <i className="fas fa-check text-success" />
+        ) : (
+          <i className="fas fa-times-circle text danger" />
+        )}
+        <div className="card grid2">
+          <div className="all-center">
+            <img
+              src={avatar_url}
+              className="round-img"
+              alt=""
+              style={{ width: "150px" }}
+            />
+            <h1>{name}</h1>
 
-  <img
-  src={avatar_url}
-  className='round-img'
-  alt=''
-  style={{width:'150px'}}
-  />
-  <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className="btn btn-dark my-1">
+              Visit Github Profile
+            </a>
+            <ul>
+              <li>
+                {login && <Fragment>
+                  <strong>Username: </strong> {login}
+                  
+                  </Fragment>}
+              </li>
+            </ul>
+            <ul>
+              <li>
+                {company && <Fragment>
+                  <strong>Company: </strong> {company}
+                  
+                  </Fragment>}
+              </li>
+            </ul>
+            <ul>
+              <li>
+                {blog && <Fragment>
+                  <strong>Website: </strong> {blog}
+                  
+                  </Fragment>}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="card text-center">
 
-  <p>Location: {location}</p>
-  </div>
-</div>
-</Fragment>
-
+          <div className="badge badge-primary">Followers:{followers}</div>
+          <div className="badge badge-success">Following:{following}</div>
+          <div className="badge badge-light">Public Repos:{public_repos}</div>
+          <div className="badge badge-dark">Public Gists:{public_gists}</div>
+        </div>
+      </Fragment>
     );
   }
 }
